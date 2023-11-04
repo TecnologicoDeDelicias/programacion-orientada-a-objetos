d7416f6e87a7224a3b29b797abef2f610e90616f
Roberto Solis <robertosolis2045@gmail.com>
2023-11-04 10:36:08 -0600

    Examen unidad 2

---
+++ Roberto-solis/unidad2/examen/Libro.py
@@ -0,0 +1,10 @@
+print ("examen")
+def _init_(self, titulo, autor, numerodeoaginas, precio, disponible):
+    self.titulo= titulo
+    self.autor = autor
+    self.numerodepaginas = numerodeoaginas
+    self.precio = precio
+    self.disponible = disponible
+    def imprimirdatos(self)
+        print("self.titulo")
+        def 
